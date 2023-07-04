c8a0bc1850091c15d77163dd21fca286a4f982da
srimanvitha <65594986+srimanvitha@users.noreply.github.com>
2023-07-05 00:10:16 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,7 +8,7 @@ def newBuild()
 }
 def newDeploy(jobname,ip,appname)
 {
-  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat/webapps/${appname}.war"
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
 }
 def runTest(jobname){
 sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar '
